cc447365bc3d41ebfe6bf84f33a0be9a3c5aaf21
zunorabj-alt <zunorabj@gmail.com>
2026-03-28 16:14:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-tulet selectedPack = "";
+let selectedPack = "";
 
 function showTab(tab){
 document.getElementById("ff").classList.add("hidden");
@@ -13,12 +13,10 @@ document.getElementById("btn-" + tab).classList.add("active");
 function openOrder(pack){
 selectedPack = pack;
 
-document.getElementById("overlay").classList.remove("hidden");
 document.getElementById("formBox").classList.remove("hidden");
 
 document.getElementById("selectedPack").innerText = "🎮 Pack: " + pack;
 
-/* 💳 PAYMENT INFO + EMOJIS */
 document.getElementById("msg").innerText =
 "📲 💳 PAIEMENT INFOS:\n\n" +
 "1️⃣ 0375348868\n   👤 HERY NIRINA HENINTSOA RAHELIARIMANANA\n\n" +
@@ -26,35 +24,29 @@ document.getElementById("msg").innerText =
 "🔰 Veuillez envoyé votre argent à l'un de ses numéro";
 }
 
-function closeOrder(){
-document.getElementById("overlay").classList.add("hidden");
-document.getElementById("formBox").classList.add("hidden");
-}
-
 function sendOrder(){
 let uid = document.getElementById("uid").value;
-  let method = document.getElementById("method").value;
-  let ref = document.getElementById("ref").value;
+let method = document.getElementById("method").value;
+let ref = document.getElementById("ref").value;
 
-  if(uid === "" || ref === ""){
-    document.getElementById("msg").innerText = "❌ Fenoy tsara ny information";
-    return;
-  }
-
-  let number = "261387508615"; // WhatsApp admin
-
-  let message =
-    "🛒 S.K Shop 💳\n\n" +
-    "🗃️ Pack: " + selectedPack + "\n" +
-    "🆔 UID: " + uid + "\n" +
-    "💳 Méthode: " + method + "\n" +
-    "🧾 Référence: " + ref + "\n\n" +
-    "💰 PAIEMENT NUMÉRO:\n" +
-    "• 0375348868 - HERY NIRINA HENINTSOA RAHELIARIMANANA\n" +
-    "• 0387508615 - Miora\n\n";
-
-  let url = "https://wa.me/" + number + "?text=" + encodeURIComponent(message);
-
-  window.open(url, "_blank");
+if(uid === "" || ref === ""){
+document.getElementById("msg").innerText = "❌ Fenoy tsara ny information";
+return;
 }
 
+let number = "261387508615";
+
+let message =
+"🛒 S.K Shop 💳\n\n" +
+"🗃️ Pack: " + selectedPack + "\n" +
+"🆔 UID: " + uid + "\n" +
+"💳 Méthode: " + method + "\n" +
+"🧾 Référence: " + ref + "\n\n" +
+"💰 PAIEMENT NUMÉRO:\n" +
+"• 0375348868 - HERY NIRINA HENINTSOA RAHELIARIMANANA\n" +
+"• 0387508615 - Miora\n\n";
+
+let url = "https://wa.me/" + number + "?text=" + encodeURIComponent(message);
+
+window.open(url, "_blank");
+  }
