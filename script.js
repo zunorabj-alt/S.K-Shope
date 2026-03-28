26f03b4dccc07de5f5fdb6c8daa3d05f61d1523f
zunorabj-alt <zunorabj@gmail.com>
2026-03-28 15:54:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 let selectedPack = "";
 
 function showTab(tab){
-  document.getElementById("ff").classList.add("hidden");
-  document.getElementById("pubg").classList.add("hidden");
+document.getElementById("ff").classList.add("hidden");
+document.getElementById("pubg").classList.add("hidden");
+document.getElementById(tab).classList.remove("hidden");
 
-  document.getElementById(tab).classList.remove("hidden");
-
-  document.getElementById("btn-ff").classList.remove("active");
-  document.getElementById("btn-pubg").classList.remove("active");
-  document.getElementById("btn-" + tab).classList.add("active");
+document.getElementById("btn-ff").classList.remove("active");
+document.getElementById("btn-pubg").classList.remove("active");
+document.getElementById("btn-" + tab).classList.add("active");
 }
 
 function openOrder(pack){
-  selectedPack = pack;
-  document.getElementById("formBox").classList.remove("hidden");
-  document.getElementById("selectedPack").innerText = pack;
+selectedPack = pack;
+
+document.getElementById("overlay").classList.remove("hidden");
+document.getElementById("formBox").classList.remove("hidden");
+
+document.getElementById("selectedPack").innerText = "🎮 Pack: " + pack;
+
+/* 💳 PAYMENT INFO + EMOJIS */
+document.getElementById("msg").innerText =
+"📲 💳 PAIEMENT INFOS:\n\n" +
+"1️⃣ 0375348868\n   👤 HERY NIRINA HENINTSOA RAHELIARIMANANA\n\n" +
+"2️⃣ 0387508615\n   👤 Miora\n\n" +
+"🔰 Veuillez envoyé votre argent à l'un de ses numéro";
+}
+
+function closeOrder(){
+document.getElementById("overlay").classList.add("hidden");
+document.getElementById("formBox").classList.add("hidden");
 }
 
 function sendOrder(){
-  let uid = document.getElementById("uid").value;
-  let method = document.getElementById("method").value;
-  let ref = document.getElementById("ref").value;
 
-  if(uid === "" || ref === ""){
-    document.getElementById("msg").innerText = "❌ Fenoy tsara";
-    return;
-  }
 
-  document.getElementById("msg").innerText = "✅ Commande envoyée!";
-    }
